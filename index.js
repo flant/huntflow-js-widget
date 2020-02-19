0ee6a16ac08c229289a9edba9830d1120dc96fc0
Anton Klimov <anton.klimov@flant.ru>
2020-02-19 14:18:21 +0300

    fixed bad file parsing error

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
 var api_prefix = 'https://job-api.flant.ru';
-function prepareApplicantData(data, $form) {      
-    var applicantData = {};
-    if (data.fields.hasOwnProperty('name') && data.fields.name != null) {
-        applicantData['last_name'] = (data.fields.name.hasOwnProperty('last') && data.fields.name.last != null && data.fields.name.last.length > 0) ? data.fields.name.last : 'Неизвестно';            
-        applicantData['first_name'] = (data.fields.name.hasOwnProperty('first') && data.fields.name.first != null && data.fields.name.first.length > 0) ? data.fields.name.first : 'Неизвестно';
-        data.fields.name.hasOwnProperty('middle') && (applicantData['middle_name'] = data.fields.name.middle);
-    } else {
-        applicantData['last_name'] = 'Неизвестно';
-        applicantData['first_name'] = 'Неизвестно';
-    }        
-    data.fields.hasOwnProperty('phones') && data.fields.phones != null && data.fields.phones.length > 0 && (applicantData['phone'] = data.fields.phones[0]);        
-    data.fields.hasOwnProperty('email') && data.fields.email != null && data.fields.email.length > 0 && (applicantData['email'] = data.fields.email);        
-    if (data.fields.hasOwnProperty('experience') && data.fields.experience != null && data.fields.experience.length > 0) {            
-        data.fields.experience[0].hasOwnProperty('position') && data.fields.experience[0].position != null && data.fields.experience[0].position.length > 0 && (applicantData['position'] = data.fields.experience[0].position);
-        data.fields.experience[0].hasOwnProperty('company') && data.fields.experience[0].company != null && data.fields.experience[0].company.length > 0 && (applicantData['company'] = data.fields.experience[0].company);                        
-    }        
-    data.fields.hasOwnProperty('salary') && data.fields.salary != null && data.fields.salary.length > 0 && (applicantData['money'] = data.fields.salary);
-    if (data.fields.hasOwnProperty('birthdate') && data.fields.birthdate != null) {
-        data.fields.birthdate.hasOwnProperty('day') && data.fields.birthdate.day != null && data.fields.birthdate.day.length > 0 && (applicantData['birthday_day'] = data.fields.birthdate.day);
-        data.fields.birthdate.hasOwnProperty('month') && data.fields.birthdate.month != null && data.fields.birthdate.month.length > 0 && (applicantData['birthday_month'] = data.fields.birthdate.month);
-        data.fields.birthdate.hasOwnProperty('year') && data.fields.birthdate.year != null && data.fields.birthdate.year.length > 0 && (applicantData['birthday_year'] = data.fields.birthdate.year);
-    }
-    data.hasOwnProperty('photo') && data.photo != null && data.photo.hasOwnProperty('id') && data.photo.id != null && data.photo.id.length > 0 && (applicantData['photo'] = data.photo.id);        
-    var applicantDataBody = 'Резюме отправлено с job.flant.ru\n' + 'Контактные данные: ' +$form.find('[name="contact"]').val() + '\n\n\n';
-    data.hasOwnProperty('text') && (applicantDataBody = applicantDataBody + data.text);
-    applicantData['externals'] = [
-        {
-            "data": {
-                "body": applicantDataBody,
-            },
-            "auth_type": "NATIVE",
-            "files": [
-                {
-                    "id": data.id
-                }
-            ]
+function prepareApplicantData(data, $form) {
+    var applicantData;
+    console.log(data);
+    if (data.fields != null) {
+        applicantData = {};
+        if (data.fields.hasOwnProperty('name') && data.fields.name != null) {
+            applicantData['last_name'] = (data.fields.name.hasOwnProperty('last') && data.fields.name.last != null && data.fields.name.last.length > 0) ? data.fields.name.last : 'Неизвестно';            
+            applicantData['first_name'] = (data.fields.name.hasOwnProperty('first') && data.fields.name.first != null && data.fields.name.first.length > 0) ? data.fields.name.first : 'Неизвестно';
+            data.fields.name.hasOwnProperty('middle') && (applicantData['middle_name'] = data.fields.name.middle);
+        } else {
+            applicantData['last_name'] = 'Неизвестно';
+            applicantData['first_name'] = 'Неизвестно';
+        }        
+        data.fields.hasOwnProperty('phones') && data.fields.phones != null && data.fields.phones.length > 0 && (applicantData['phone'] = data.fields.phones[0]);        
+        data.fields.hasOwnProperty('email') && data.fields.email != null && data.fields.email.length > 0 && (applicantData['email'] = data.fields.email);        
+        if (data.fields.hasOwnProperty('experience') && data.fields.experience != null && data.fields.experience.length > 0) {            
+            data.fields.experience[0].hasOwnProperty('position') && data.fields.experience[0].position != null && data.fields.experience[0].position.length > 0 && (applicantData['position'] = data.fields.experience[0].position);
+            data.fields.experience[0].hasOwnProperty('company') && data.fields.experience[0].company != null && data.fields.experience[0].company.length > 0 && (applicantData['company'] = data.fields.experience[0].company);                        
+        }        
+        data.fields.hasOwnProperty('salary') && data.fields.salary != null && data.fields.salary.length > 0 && (applicantData['money'] = data.fields.salary);
+        if (data.fields.hasOwnProperty('birthdate') && data.fields.birthdate != null) {
+            data.fields.birthdate.hasOwnProperty('day') && data.fields.birthdate.day != null && data.fields.birthdate.day.length > 0 && (applicantData['birthday_day'] = data.fields.birthdate.day);
+            data.fields.birthdate.hasOwnProperty('month') && data.fields.birthdate.month != null && data.fields.birthdate.month.length > 0 && (applicantData['birthday_month'] = data.fields.birthdate.month);
+            data.fields.birthdate.hasOwnProperty('year') && data.fields.birthdate.year != null && data.fields.birthdate.year.length > 0 && (applicantData['birthday_year'] = data.fields.birthdate.year);
         }
-    ];
+        data.hasOwnProperty('photo') && data.photo != null && data.photo.hasOwnProperty('id') && data.photo.id != null && data.photo.id.length > 0 && (applicantData['photo'] = data.photo.id);        
+        var applicantDataBody = 'Резюме отправлено с job.flant.ru\n' + 'Контактные данные: ' +$form.find('[name="contact"]').val() + '\n\n\n';
+        data.hasOwnProperty('text') && (applicantDataBody = applicantDataBody + data.text);
+        applicantData['externals'] = [
+            {
+                "data": {
+                    "body": applicantDataBody,
+                },
+                "auth_type": "NATIVE",
+                "files": [
+                    {
+                        "id": data.id
+                    }
+                ]
+            }
+        ];
+    } else {
+        applicantData = null;
+    }
     return applicantData;
 }
 
@@ -63,55 +69,60 @@ function sendForm($form) {
                 console.log('file uploaded');
                 var applicantData = prepareApplicantData(parsedFileDataResponse, $form);
                 console.log(applicantData);
-                $.ajax({
-                    url: api_prefix + '/api/vacancy_request',
-                    data: JSON.stringify(applicantData),
-                    contentType: "application/json; charset=utf-8",
-                    cache: false,
-                    processData: false,
-                    method: 'POST',
-                    type: 'POST',
-                    success: function(applicationResponse){
-                        console.log('application success');
-                        console.log(applicationResponse);
-                        $form.addClass('success');
-                        if (vacancy_id != '') {
-                            var vacancyData = {
-                                "vacancy": vacancy_id,
-                                "status": status_id,
-                                "comment": "Кандидат добавлен на вакансию через заявку с job.flant.ru",
-                                "files": [
-                                    {
-                                        "id": applicantData.externals[0].files[0].id
-                                    }
-                                ],
-                                "rejection_reason": null
-                            }
-                            $.ajax({
-                                url: api_prefix + '/api/vacancy_request/' + applicationResponse.id,
-                                data: JSON.stringify(vacancyData),
-                                contentType: "application/json; charset=utf-8",
-                                cache: false,
-                                processData: false,
-                                method: 'POST',
-                                type: 'POST',
-                                success: function(vacancyResponse){
-                                    console.log('vacancy success');
-                                    console.log(vacancyResponse);
-                                },
-                                error: function(vacancyResponse) {
-                                    console.log('vacancy failed');
-                                    console.log(vacancyResponse);
+                if (applicantData != null) {
+                    $.ajax({
+                        url: api_prefix + '/api/vacancy_request',
+                        data: JSON.stringify(applicantData),
+                        contentType: "application/json; charset=utf-8",
+                        cache: false,
+                        processData: false,
+                        method: 'POST',
+                        type: 'POST',
+                        success: function(applicationResponse){
+                            console.log('application success');
+                            console.log(applicationResponse);
+                            $form.addClass('success');
+                            if (vacancy_id != '') {
+                                var vacancyData = {
+                                    "vacancy": vacancy_id,
+                                    "status": status_id,
+                                    "comment": "Кандидат добавлен на вакансию через заявку с job.flant.ru",
+                                    "files": [
+                                        {
+                                            "id": applicantData.externals[0].files[0].id
+                                        }
+                                    ],
+                                    "rejection_reason": null
                                 }
-                            });
+                                $.ajax({
+                                    url: api_prefix + '/api/vacancy_request/' + applicationResponse.id,
+                                    data: JSON.stringify(vacancyData),
+                                    contentType: "application/json; charset=utf-8",
+                                    cache: false,
+                                    processData: false,
+                                    method: 'POST',
+                                    type: 'POST',
+                                    success: function(vacancyResponse){
+                                        console.log('vacancy success');
+                                        console.log(vacancyResponse);
+                                    },
+                                    error: function(vacancyResponse) {
+                                        console.log('vacancy failed');
+                                        console.log(vacancyResponse);
+                                    }
+                                });
+                            }
+                        },
+                        error: function(applicationResponse) {
+                            console.log('application failed');
+                            console.log(applicationResponse);
+                            $form.addClass('error');
                         }
-                    },
-                    error: function(applicationResponse) {
-                        console.log('application failed');
-                        console.log(applicationResponse);
-                        $form.addClass('error');
-                    }
-                });  
+                    });
+                } else {
+                    console.log('file parse failed');
+                    $form.addClass('error');
+                }                  
             },
             error: function() {
                 console.log('file upload failed');                
