13258654253f40988be0f04f1f9fb6e4ffb0b9fb
Anton Klimov <anton.klimov@flant.ru>
2020-02-19 16:04:59 +0300

    better error handling

--- index.js
+++ index.js
@@ -1,49 +1,68 @@
 var api_prefix = 'https://job-api.flant.ru';
-function prepareApplicantData(data, $form) {
+function prepareApplicantData(data, $form) {    
     var applicantData;
     console.log(data);
-    if (data.fields != null) {
-        applicantData = {};
-        if (data.fields.hasOwnProperty('name') && data.fields.name != null) {
-            applicantData['last_name'] = (data.fields.name.hasOwnProperty('last') && data.fields.name.last != null && data.fields.name.last.length > 0) ? data.fields.name.last : 'Неизвестно';            
-            applicantData['first_name'] = (data.fields.name.hasOwnProperty('first') && data.fields.name.first != null && data.fields.name.first.length > 0) ? data.fields.name.first : 'Неизвестно';
-            data.fields.name.hasOwnProperty('middle') && (applicantData['middle_name'] = data.fields.name.middle);
-        } else {
-            applicantData['last_name'] = 'Неизвестно';
-            applicantData['first_name'] = 'Неизвестно';
-        }        
-        data.fields.hasOwnProperty('phones') && data.fields.phones != null && data.fields.phones.length > 0 && (applicantData['phone'] = data.fields.phones[0]);        
-        data.fields.hasOwnProperty('email') && data.fields.email != null && data.fields.email.length > 0 && (applicantData['email'] = data.fields.email);        
-        if (data.fields.hasOwnProperty('experience') && data.fields.experience != null && data.fields.experience.length > 0) {            
-            data.fields.experience[0].hasOwnProperty('position') && data.fields.experience[0].position != null && data.fields.experience[0].position.length > 0 && (applicantData['position'] = data.fields.experience[0].position);
-            data.fields.experience[0].hasOwnProperty('company') && data.fields.experience[0].company != null && data.fields.experience[0].company.length > 0 && (applicantData['company'] = data.fields.experience[0].company);                        
-        }        
-        data.fields.hasOwnProperty('salary') && data.fields.salary != null && data.fields.salary.length > 0 && (applicantData['money'] = data.fields.salary);
-        if (data.fields.hasOwnProperty('birthdate') && data.fields.birthdate != null) {
-            data.fields.birthdate.hasOwnProperty('day') && data.fields.birthdate.day != null && data.fields.birthdate.day.length > 0 && (applicantData['birthday_day'] = data.fields.birthdate.day);
-            data.fields.birthdate.hasOwnProperty('month') && data.fields.birthdate.month != null && data.fields.birthdate.month.length > 0 && (applicantData['birthday_month'] = data.fields.birthdate.month);
-            data.fields.birthdate.hasOwnProperty('year') && data.fields.birthdate.year != null && data.fields.birthdate.year.length > 0 && (applicantData['birthday_year'] = data.fields.birthdate.year);
-        }
-        data.hasOwnProperty('photo') && data.photo != null && data.photo.hasOwnProperty('id') && data.photo.id != null && data.photo.id.length > 0 && (applicantData['photo'] = data.photo.id);        
-        var applicantDataBody = 'Резюме отправлено с job.flant.ru\n' + 'Контактные данные: ' +$form.find('[name="contact"]').val() + '\n\n\n';
-        data.hasOwnProperty('text') && (applicantDataBody = applicantDataBody + data.text);
-        applicantData['externals'] = [
-            {
-                "data": {
-                    "body": applicantDataBody,
-                },
-                "auth_type": "NATIVE",
-                "files": [
-                    {
-                        "id": data.id
-                    }
-                ]
+    try {
+        if (data.fields != null) {
+            applicantData = {};
+            if (data.fields.hasOwnProperty('name') && data.fields.name != null) {
+                applicantData['last_name'] = (data.fields.name.hasOwnProperty('last') && data.fields.name.last != null && data.fields.name.last.length > 0) ? data.fields.name.last : 'Неизвестно';            
+                applicantData['first_name'] = (data.fields.name.hasOwnProperty('first') && data.fields.name.first != null && data.fields.name.first.length > 0) ? data.fields.name.first : 'Неизвестно';
+                data.fields.name.hasOwnProperty('middle') && (applicantData['middle_name'] = data.fields.name.middle);
+            } else {
+                applicantData['last_name'] = 'Неизвестно';
+                applicantData['first_name'] = 'Неизвестно';
+            }        
+            data.fields.hasOwnProperty('phones') && data.fields.phones != null && data.fields.phones.length > 0 && (applicantData['phone'] = data.fields.phones[0]);        
+            data.fields.hasOwnProperty('email') && data.fields.email != null && data.fields.email.length > 0 && (applicantData['email'] = data.fields.email);        
+            if (data.fields.hasOwnProperty('experience') && data.fields.experience != null && data.fields.experience.length > 0) {            
+                data.fields.experience[0].hasOwnProperty('position') && data.fields.experience[0].position != null && data.fields.experience[0].position.length > 0 && (applicantData['position'] = data.fields.experience[0].position);
+                data.fields.experience[0].hasOwnProperty('company') && data.fields.experience[0].company != null && data.fields.experience[0].company.length > 0 && (applicantData['company'] = data.fields.experience[0].company);                        
+            }        
+            data.fields.hasOwnProperty('salary') && data.fields.salary != null && data.fields.salary.length > 0 && (applicantData['money'] = data.fields.salary);
+            if (data.fields.hasOwnProperty('birthdate') && data.fields.birthdate != null) {
+                data.fields.birthdate.hasOwnProperty('day') && data.fields.birthdate.day != null && data.fields.birthdate.day.length > 0 && (applicantData['birthday_day'] = data.fields.birthdate.day);
+                data.fields.birthdate.hasOwnProperty('month') && data.fields.birthdate.month != null && data.fields.birthdate.month.length > 0 && (applicantData['birthday_month'] = data.fields.birthdate.month);
+                data.fields.birthdate.hasOwnProperty('year') && data.fields.birthdate.year != null && data.fields.birthdate.year.length > 0 && (applicantData['birthday_year'] = data.fields.birthdate.year);
             }
-        ];
-    } else {
-        applicantData = null;
+            data.hasOwnProperty('photo') && data.photo != null && data.photo.hasOwnProperty('id') && data.photo.id != null && data.photo.id.length > 0 && (applicantData['photo'] = data.photo.id);        
+            var applicantDataBody = 'Резюме отправлено с job.flant.ru\n' + 'Контактные данные: ' +$form.find('[name="contact"]').val() + '\n\n\n';
+            data.hasOwnProperty('text') && (applicantDataBody = applicantDataBody + data.text);
+            applicantData['externals'] = [
+                {
+                    "data": {
+                        "body": applicantDataBody,
+                    },
+                    "auth_type": "NATIVE",
+                    "files": [
+                        {
+                            "id": data.id
+                        }
+                    ]
+                }
+            ];
+        } else {
+            applicantData = null;
+        }
+        return applicantData;
     }
-    return applicantData;
+    catch (e) {
+        handleFormError($form, 'application data parsing went wrong', e);
+    }    
+}
+
+function handleFormError($form, m, e) {
+    $form.addClass('sent');
+    $form.addClass('error');
+    m && console.log(m);
+    e && console.log(e);
+    if (typeof Sentry !== 'undefined') {
+        Sentry.configureScope(function(scope) {
+            m && scope.setExtra('Report', m);
+            scope.setExtra('Contact', $form.find('[name="contact"]').val());
+        });
+        e && Sentry.captureException(e);
+    }        
 }
 
 var formNotificationTimeout;
@@ -107,26 +126,21 @@ function sendForm($form) {
                                         console.log(vacancyResponse);
                                     },
                                     error: function(vacancyResponse) {
-                                        console.log('vacancy failed');
-                                        console.log(vacancyResponse);
+                                        handleFormError($form, 'vacancy failed', vacancyResponse)
                                     }
                                 });
                             }
                         },
                         error: function(applicationResponse) {
-                            console.log('application failed');
-                            console.log(applicationResponse);
-                            $form.addClass('error');
+                            handleFormError($form, 'application failed', applicationResponse)
                         }
                     });
                 } else {
-                    console.log('file parse failed');
-                    $form.addClass('error');
+                    handleFormError($form, 'file parse failed: empty data');
                 }                  
             },
-            error: function() {
-                console.log('file upload failed');                
-                $form.addClass('error');
+            error: function(parsedFileDataResponse) {
+                handleFormError($form, 'file upload failed', parsedFileDataResponse);
             }
         });                                  
     } else {
@@ -140,14 +154,7 @@ function sendForm($form) {
 
 $(document).on('click tap', '.hr-form-submit', function(e) {
     e.preventDefault();
-    var $form = $(this).closest('.hr-form');
-    try {        
-        sendForm($form);
-    } catch (err) {
-        $form.addClass('sent');
-        $form.addClass('error');
-        console.log(err);    
-    }
+    sendForm($(this).closest('.hr-form'));    
 });
 
 $(document).on('click tap', '.hr-form__upload .hr-form__button', function(e) {
